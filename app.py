88d02f05643e11d48d25d0b2cc09e5772dfa4571
Harshit4603 <harshit4603.4.6.00@gmail.com>
2026-05-08 14:47:01 +0530

    Update print statement from 'Hello' to 'Goodbye'

--- app.py
+++ app.py
@@ -80,6 +80,9 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
+    "https://ui-index-html.vercel.app",
+    "https://ui-index-html-git-main-harshit4603s-projects.vercel.app",
+    "https://ui-index-html-85hplh849-harshit4603s-projects.vercel.app",
     "https://ui-index-html-evco4h9ri-harshit4603s-projects.vercel.app",
     "http://localhost:3000",
     ],
